8d28382aec6668aa8edc15ea2d4345cb8dc0c0eb
yogita20d <60918863+yogita20d@users.noreply.github.com>
2020-02-12 09:54:52 +0000

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -27,7 +27,7 @@ public class Stepdefs {
 	static int numberOfProducts; 
 	@Given("Alex is on the login page")
 	public void alex_is_on_the_login_page() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\Drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "‪C:\Users\Training1\Downloads\Selenium\chromedriver_win32_Latest");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
